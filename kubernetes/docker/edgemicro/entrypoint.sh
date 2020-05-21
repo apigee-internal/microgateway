a1d539e4ad497e3512558bd69df53d20c3df1c3f
Gustavo Hoirisch <gugahoi@users.noreply.github.com>
2020-05-21 14:10:40 +1000

    Quote EDGEMICRO_CONFIG to properly decode it's contents
    Without quoting this variable new lines get incorrectly printed and
    consequently base64 decoding is incorrect.

--- kubernetes/docker/edgemicro/entrypoint.sh
+++ kubernetes/docker/edgemicro/entrypoint.sh
@@ -31,7 +31,7 @@ start_edge_micro() {
 
   if [[ -n "$EDGEMICRO_CONFIG"  ]]
     then
-      echo $EDGEMICRO_CONFIG | base64 -d > ${APIGEE_ROOT}/.edgemicro/$EDGEMICRO_ORG-$EDGEMICRO_ENV-config.yaml
+      echo "$EDGEMICRO_CONFIG" | base64 -d > ${APIGEE_ROOT}/.edgemicro/$EDGEMICRO_ORG-$EDGEMICRO_ENV-config.yaml
       chown apigee:apigee ${APIGEE_ROOT}/.edgemicro/*
   fi
 
