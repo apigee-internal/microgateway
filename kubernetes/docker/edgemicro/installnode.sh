b31388dc425d83b42ae5408d9511c866d21899ec
Tapas Thakkar <tapasthakkar@gmail.com>
2026-08-02 14:47:35 +0530

    541758106 Update Node.js base image to 24.18.1

--- kubernetes/docker/edgemicro/installnode.sh
+++ kubernetes/docker/edgemicro/installnode.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Allow installing dependencies directly from Git repositories (npm v12+)
+npm config set allow-git all --global || true
+
 # 1. Install globally (this line is targeted by betabuild.sh's sed replacement)
 npm install --omit=dev --omit=optional -g edgemicro
 
